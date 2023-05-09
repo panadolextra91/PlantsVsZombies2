fc8bbf05f94ac3cf82b4767bf75bf0899e7b9a58
Phạm Nguyễn Đăng Khôi <88122862+dangkhoi3107@users.noreply.github.com>
2023-05-09 21:25:28 +0700

    Update Collider.java

--- src/Collider.java
+++ src/Collider.java
@@ -1,3 +1,4 @@
+// Imports necessary libraries for the Swing components and event handling
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
@@ -7,64 +8,76 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 /**
- * Created by Armin on 6/25/2016.
+ * Collider is a custom Swing component that represents a plant collider in a game.
  */
 public class Collider extends JPanel implements MouseListener {
 
+    // Declare an ActionListener for handling events
     ActionListener al;
 
+    // Constructor for the Collider class
     public Collider(){
-        //setBorder(new LineBorder(Color.RED));
+
+        // Make the collider transparent
         setOpaque(false);
+        
+        // Register this object as a MouseListener
         addMouseListener(this);
-        //setBackground(Color.green);
+
+        // Set the size of the collider to 100x120 pixels
         setSize(100,120);
     }
 
+    // Declare a Plant object to represent the assigned plant
     public Plant assignedPlant;
 
+    // Method to set the assigned plant
     public void setPlant(Plant p){
         assignedPlant = p;
     }
 
+    // Method to remove the assigned plant and stop its actions
     public void removePlant(){
         assignedPlant.stop();
         assignedPlant = null;
     }
 
+    // Method to check if a point (tx) is inside the collider bounds
     public boolean isInsideCollider(int tx){
         return (tx > getLocation().x) && (tx < getLocation().x + 100);
     }
 
+    // Method to set the ActionListener for the collider
     public void setAction(ActionListener al){
         this.al = al;
     }
 
-
+    // Empty method for the mouseClicked event
     @Override
     public void mouseClicked(MouseEvent e) {
-
     }
 
+    // Empty method for the mousePressed event
     @Override
     public void mousePressed(MouseEvent e) {
-
     }
 
+    // Method for the mouseReleased event
     @Override
     public void mouseReleased(MouseEvent e) {
+        // If an ActionListener is set, trigger the actionPerformed event
         if(al != null){
             al.actionPerformed(new ActionEvent(this,ActionEvent.RESERVED_ID_MAX+1,""));
         }
     }
 
+    // Empty method for the mouseEntered event
     @Override
     public void mouseEntered(MouseEvent e) {
-
     }
 
+    // Empty method for the mouseExited event
     @Override
     public void mouseExited(MouseEvent e) {
-
     }
 }
