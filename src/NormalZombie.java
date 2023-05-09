9965749a6ac6338babf4372d8b7278fb4bb95f99
Phạm Nguyễn Đăng Khôi <88122862+dangkhoi3107@users.noreply.github.com>
2023-05-09 22:29:37 +0700

    Update NormalZombie.java

--- src/NormalZombie.java
+++ src/NormalZombie.java
@@ -1,6 +1,3 @@
-/**
- * Created by Armin on 6/25/2016.
- */
 import javax.swing.ImageIcon;
 import java.awt.*;
 import java.util.Random;
@@ -9,6 +6,7 @@ class NormalZombie extends Zombie {
     //private static final Image NORMAL_ZOMBIE_IMAGE = new ImageIcon(NormalZombie.class.getResource("images/zombies/zombie1.gif")).getImage();
     //private static final Image CONE_HEAD_ZOMBIE_IMAGE = new ImageIcon(NormalZombie.class.getResource("images/zombies/zombie2.gif")).getImage();
 
+    // Constructor for NormalZombie
     public NormalZombie(GamePanel parent, int lane) {
         super(parent, lane);
         Random random = new Random();
@@ -18,6 +16,7 @@ class NormalZombie extends Zombie {
         setImage(index == 0 ? parent.normalZombieImage : parent.coneHeadZombieImage);
     }
 
+    // Method to set the image for the zombie
     private void setImage(Image image) {
         // Set the image for the zombie
     }
