3813f797b15f8904d6f6e5e6696139957d4b5d0f
DInh Hoang Thao Nguyen <86816697+StephanieHwang421@users.noreply.github.com>
2023-05-22 13:43:12 +0700

    Update Cactus.java

--- src/Cactus.java
+++ src/Cactus.java
@@ -1,2 +1,29 @@
-public class Cactus {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class Cactus extends Plants{
+  // Timer that controls the shooting intervals of the Fume Shroom
+    public Timer shootTimer;
+
+    // Constructor for the FumeShroom class
+    public Cactus(GamePanel parent, int x, int y) {
+        // Call the superclass constructor
+        super(parent, x, y, 300);
+
+        // Initialize and start the shootTimer with a 2000ms (2 seconds) interval
+        shootTimer = new Timer(2000, (ActionEvent e) -> {
+            // If there are zombies in the same lane as the Fume Shroom
+            if(gp.laneZombies.get(y).size() > 0) {
+                // Create and add a new Puff to the lane
+                gp.lanePeas.get(y).add(new Spike(gp, y, 103 + this.x * 115));
+            }
+        });
+        shootTimer.start();
+    }
+
+    // Override the stop method to stop the shootTimer
+    @Override
+    public void stop(){
+        shootTimer.stop();
+    }
 }
